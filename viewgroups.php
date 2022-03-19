e5fe3c7064f60bb82997c040296150348c548ca6
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-03-19 10:22:49 -0600

    Update viewgroups.php

--- viewgroups.php
+++ viewgroups.php
@@ -61,7 +61,7 @@ $q9 = "SELECT Ranks.rankid FROM Ranks, Profile WHERE Profile.profile_id = '$id'"
 $r9 = $db->query($q9);
 while ( $rankrow = $r9->fetch_assoc()) {
 $rankt =  $rankrow["rankid"];
-$q1 = "SELECT  Groups.groupname,Groups.group_id Groups.groupdescription FROM Groups, Ranks, Members WHERE Groups.ranktag LIKE '%$rankt%'";
+$q1 = "SELECT  Groups.groupname,Groups.group_id Groups.groupdescription, Groups.ranktag FROM Groups,  WHERE Groups.ranktag LIKE '%$rankt%'";
 $result = mysqli_query($db,$q1);
  while($row = mysqli_fetch_assoc($result)) {   
 $groupN = $result["groupname"];
