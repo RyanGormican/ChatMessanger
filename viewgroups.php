38a94bf0c8162ea46e9d65113ddf8a192f97e3bc
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-03-19 01:45:38 -0600

    Update viewgroups.php

--- viewgroups.php
+++ viewgroups.php
@@ -7,7 +7,7 @@
    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" type = "text/css" href="assignment.css">
+<link rel="stylesheet" type = "text/css" href="project.css">
 <script src="https://kit.fontawesome.com/cb59c9bd28.js" crossorigin="anonymous"></script>
 <script src='signup.js'></script>
     <title>
@@ -23,7 +23,7 @@
     <img class = "groupimages" src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png">
     </td>
     <td>
-<i class="fa-solid fa-book-atlas"></i> Take Test
+<i class="fa-solid fa-book-atlas"></i> <a href="testoverview.php" >  Take Test </a> 
     </td>
     </tr>
     <tr>
@@ -51,9 +51,6 @@
        </td>
        </tr>
        </table>
-       </td>
-      <td class = "borderelement2" id = "nameborder" colspan = "7" rowspan = "5">
-        <textarea id="groupdescription" class = "description"  name="description"></textarea>
        </td>
       </tr>
       <tr>
@@ -119,12 +116,6 @@
    <a href="index.php"> Logout </a>
   </td>
   <td colspan = "5">
-  </td>
-  <td>
-     <textarea id="chatmes" class = "meswidth"  name="message"></textarea>
-  </td>  
-  <td>
-       <input type="submit" value ="Send Message"> 
   </td>
   <td>
 <i class="fa-solid fa-circle-play"></i>
