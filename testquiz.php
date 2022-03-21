4e2908d3b33435cdc4a77c7af3937b323844840f
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-03-21 13:01:00 -0600

    Update testquiz.php

--- testquiz.php
+++ testquiz.php
@@ -86,7 +86,7 @@ if ($correctanswer >= $needright)
     $ranking2 = $languagedif; 
     $q10 = "UPDATE RANKS SET Ranks.rankid= '$ranking2' WHERE Ranks.profile_id = '$id' AND Ranks.ranklanguage = '$languagename'";
      $updatequery = mysqli_query($db,$q10);
-echo "<script>alert('Congratulations you passed and your rank has now increased!');window.location.href='testoverview.php;</script>";
+echo "<script>alert('Congratulations you passed and your rank has now increased!');window.location.href='testoverview.php';</script>";
     }
 else
 {
